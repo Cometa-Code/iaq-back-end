b2aea2c525a378c5b2e1cb2695d1e4f8c2082a37
Vitor Barroso <vitorlauvresbarroso@gmail.com>
2024-05-16 01:37:24 -0300

    .

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('principal_document')->unique();
+            $table->enum('role', ['youngapprentice', 'company', 'supervisor', 'admin', 'superadmin'])->default('youngapprentice');
+            $table->boolean('is_active')->default(true);
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
